fa959d6cb2ca29ea11d5bd952515cdec05bd501f
Brett <bshearrer11@gmail.com>
2021-05-01 14:55:54 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Walnut Salad Bowl',
+            price: 55,
+            image:
+                'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1stopretroshop.com%2Fitem-photos%2Fbig-round-walnut-wood-bowl-for-salad-fruit-60s-vintage-danish-modern-1stopretroshop-n2939-1.jpg&f=1&nofb=1',
+        },
+        {
+            id: '2',
+            name: 'Curly Cherry Charcuterie Board',
+            price: 65,
+            image:
+                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1798%2F6235%2Fproducts%2F9581A3C7-079B-474E-9450-21F36125D9A9_1.JPEG%3Fv%3D1547497957&f=1&nofb=1',
+        },
+        {
+            id: '3',
+            name: 'Custom French Farm Table',
+            price: 1500,
+            image:
+                'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.oldplank.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F1%2Fimage%2F9df78eab33525d08d6e5fb8d27136e95%2Fo%2Fp%2Fopc11.jpg&f=1&nofb=1',
+        },
+    ],
+}
+export default data
